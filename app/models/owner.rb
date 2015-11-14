85240b9992738991af749ddbb31d58f46c4554b9
Galen Burghardt <galen.w.burghardt@gmail.com>
2015-11-14 11:40:01 -0600

    Updated owner model

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,6 +1,7 @@
 class Owner < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable
+
+  has_many :restaurants
 end
